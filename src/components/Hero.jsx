1680124cc7e303ab64fb51fdc07fa987bc772611
Naveen <naveennaveen39888@example.com>
2026-04-14 18:56:33 +0530

    responsive added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,9 @@ function Hero() {
     <div className='hero'>
         <h1 >About Me</h1>
     <div className='hero-container'>
+        <div>
+        <img src={myimg} alt="" style={{borderRadius:"60px",marginTop:"10px"}} height={300} width={300}/>
+       </div>
        <div className='hero-content'>
          <p style={{padding:"0px",margin:"0px",color:"black"}}>Hi There!</p>
         <h1> I'm <span>Naveen</span></h1>
@@ -14,9 +17,7 @@ function Hero() {
         <h3>Enthusiastic beginner with a strong foundation in full-stack and Java development. Passionate about building real-world projects, continuously learning, and contributing to team and organizational growth.</h3>
         <div className='button'>Hire Me</div>
        </div>
-       <div>
-        <img src={myimg} alt="" style={{borderRadius:"60px",marginTop:"10px"}} height={300} width={300}/>
-       </div>
+       
     </div>
     </div>
       )
